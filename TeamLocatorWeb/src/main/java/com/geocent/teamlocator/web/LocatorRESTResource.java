4ae57c7a27bfde7d957fd32ece9039095b4440f4
Randy Nolen <randy.nolen@geocent.com>
2012-12-20 13:49:15 -0500

    Implemented first REST operation and added TeamLocatorCommon as dependency.

--- TeamLocatorWeb/src/main/java/com/geocent/teamlocator/web/LocatorRESTResource.java
+++ TeamLocatorWeb/src/main/java/com/geocent/teamlocator/web/LocatorRESTResource.java
@@ -1,11 +1,31 @@
 package com.geocent.teamlocator.web;
 
-import javax.ejb.Stateless;
-import javax.ws.rs.Path;
+import java.util.List;
 
-@Path("method")
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+import com.geocent.teamlocator.dto.TeamDto;
+import com.geocent.teamlocator.service.TeamLocatorService;
+
+@Path("locator")
 @Stateless
 public class LocatorRESTResource
 {
-
+    @EJB
+    private TeamLocatorService service;
+    
+    @GET
+    @Path( "getTeamByName" )
+    @Produces( {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML} )
+    public List<TeamDto> getTeamByName( @QueryParam(value="name") String name ) {
+        System.out.println( "---->DEBUG: LocatorRESTResource.getTeamByName: name=" + name );
+        return service.getTeamByName( name );
+    }
 }
